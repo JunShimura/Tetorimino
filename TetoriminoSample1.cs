1be5a787d6c0f4d976d6d2c20c2f9cd5b6ebfa51
Jun Shimura <junshimura@gmail.com>
2020-06-29 11:56:39 +0900

    プロジェクト ファイルを追加します。

---
+++ TetoriminoSample1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Tetorimino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            char[] t = new char[7] { 'I', 'O', 'T', 'J', 'L', 'S', 'Z' };
+            //全部を用意すると5040個の配列を用意すると大変
+            string[] minoTable = new string[50000];
+            Random random = new Random();
+            for (int i = 0; i < minoTable.Length; i++)
+            {   //ミノの順番を出力する
+                List<char> minoList = new List<char>(t);
+                for (int j = t.Length; j > 0; j--)
+                {   //残りｊ文字から1文字取り出して繋げる
+                    int r = random.Next(0, j);
+                    minoTable[i] += minoList[r];
+                    //一度引いた要素を取り除く
+                    minoList.RemoveAt(r);
+                }
+            }
+            sw.Stop();
+            Console.WriteLine("かかった時間は{0}", sw.Elapsed);
+
+            /*
+            // 結果を出力
+            for (int i = 0; i < minoTable.Length; i++)
+            {
+                Console.WriteLine("{0}番目：{1}", i, minoTable[i]);
+            }
+            */
+        }
+    }
+}
